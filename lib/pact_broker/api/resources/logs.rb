4ac453444d277b4ef0e061b5e2b4d780bea0caf2
matthewwe <matthew_weisfeld@ultimatesoftware.com>
2020-07-09 15:14:21 -0400

    Added fields to verfications table and endpoints.

---
+++ lib/pact_broker/api/resources/logs.rb
@@ -0,0 +1,54 @@
+require 'pact_broker/api/resources/base_resource'
+require 'pact_broker/configuration'
+require 'pact_broker/domain/verification'
+require 'pact_broker/api/contracts/verification_contract'
+require 'pact_broker/api/decorators/verification_decorator'
+require 'pact_broker/api/decorators/extended_verification_decorator'
+
+module PactBroker
+  module Api
+    module Resources
+      class Logs < BaseResource
+        def content_types_provided
+          [
+            ["application/hal+json", :to_json],
+            ["application/json", :to_json]
+          ]
+        end
+
+        # Remember to update latest_verification_id_for_pact_version_and_provider_version
+        # if/when DELETE is implemented
+        def allowed_methods
+          ["GET", "OPTIONS"]
+        end
+
+        def resource_exists?
+          if identifier_from_path[:verification_number] == "all"
+            set_json_error_message("To see all the verifications for a pact, use the Matrix page")
+            false
+          else
+            !!verification
+          end
+        end
+
+        def to_json
+          decorator_for(verification).to_json(user_options: { base_url: base_url })
+        end
+
+        def to_extended_json
+          extended_decorator_for(verification).to_json(user_options: { base_url: base_url })
+        end
+
+        private
+
+        def verification
+          @verification ||= verification_service.find_by_logs_id(identifier_from_path)
+        end
+
+        def decorator_for model
+          PactBroker::Api::Decorators::VerificationDecorator.new(model)
+        end
+      end
+    end
+  end
+end
